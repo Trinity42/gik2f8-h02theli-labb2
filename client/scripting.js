ac5b353acb9148c8c72e9a604d9f0ad9532772f6
Therese Lissel <therese.lissel@gmail.com>
2022-12-25 23:42:29 +0100

    tog bort fil scripting

--- client/scripting.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// const clearForm = document.getElementById('clearForm');
-// console.log(clearForm);
-
-// clearForm.addEventListener('click', () => {
-//   todoForm.reset();
-// });
-// &#9989;
